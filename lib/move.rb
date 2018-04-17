3ff286cbcfc987b16a2018811e936300e79fbaae
Daniel Doughty <djdoughty@gmail.com>
2018-04-17 16:02:03 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,6 @@ def input_to_index (input)
   return index
 end
 
-def move (board, index, character = "X")
-  board[index] = character
+def move (board, b, character = "X")
+  board[b] = character
 end
